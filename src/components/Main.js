28d789a1f5a8f079dc1afb79b2b101a914a58ad6
Seetaram Yadav <in-seetaram.yadav@Seetarams-MacBook-Pro-2.local>
2020-01-17 22:45:04 +0530

    Added the card-thumb image.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import ProductUrl from "../../images/product-image.png";
+import ProductUrl from "../../images/cart-thumb.jpg";
 
 const MainContainer = styled.main``;
 
 const Product = styled.ul``;
 
 export const Main = () => {
-  const products = ["one ", "second", "thired"];
+  const products = ["one ", "2", "3", "j4", "h5", "6", "j7", "8o", "9"];
   return (
     <MainContainer>
       {products.map((product, index) => {
